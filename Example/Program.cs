179edae171b18da35249f11efa22c4a5a8166205
Matt Jarvis <mattj23@gmail.com>
2026-01-30 18:17:07 -0500

    test running

--- Example/Program.cs
+++ Example/Program.cs
@@ -3,12 +3,16 @@
 using System.Diagnostics;
 using NicolaySerialSFM3x00;
 
-Console.WriteLine("Starting");
 var device = new SfmDevice("/dev/ttyUSB0");
-device.Connect();
+Console.WriteLine("Connecting...");
+await device.Connect();
 
-device.Check();
-await Task.Delay(1000);
+if (!await device.Check())
+{
+    Console.WriteLine("Error, device check failed!");
+    return;
+}
+Console.WriteLine("Device check passed, starting measurements for 10 seconds...");
 
 var stopwatch = new Stopwatch();
 stopwatch.Start();
@@ -16,10 +20,17 @@ stopwatch.Start();
 var count = 0;
 while (stopwatch.ElapsedMilliseconds < 10000)
 {
-    var _ = await device.GetValue();
+    var raw = await device.GetValue();
+    var value = (raw - 32768) / 120.0;
+    // Console.WriteLine($"Measurement: {value:F2} ml/min");
+    
     count++;
 }
+
 stopwatch.Stop();
 Console.WriteLine($"Received {count} measurements in {stopwatch.ElapsedMilliseconds} ms ({count / (stopwatch.ElapsedMilliseconds / 1000.0)} Hz)");
+Console.WriteLine($"(Took approx {stopwatch.ElapsedMilliseconds / (double)count:F2}ms per measurement)");
+
+await device.DisposeAsync();
 
 
--- NicolaySerialSFM3x00/SfmDevice.cs
+++ NicolaySerialSFM3x00/SfmDevice.cs
@@ -1,19 +1,26 @@
 using System;
 using System.IO.Ports;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NicolaySerialSFM3x00
 {
-    public class SfmDevice : IDisposable
+    public class SfmDevice : IDisposable, IAsyncDisposable
     {
         private readonly string _portName;
         private readonly byte _address;
         
         private SerialPort _serialPort;
-        private readonly byte[] _rxBuffer = new byte[4086];
-        private TaskCompletionSource<int> _measurementTcs;
-
         private readonly RxParser _rxParser;
+        
+        // Signals and tasks for starting/stopping the serial communication
+        private readonly CancellationTokenSource _cts = new();
+        private TaskCompletionSource<bool> _stopReadTcs;
+        private TaskCompletionSource<bool> _startTcs;
+
+        // Task completion sources for response requests
+        private TaskCompletionSource<int> _measurementTcs;
+        private TaskCompletionSource<bool> _checkTcs;
 
         public SfmDevice(string portName, byte address = 0x01)
         {
@@ -22,19 +29,30 @@ namespace NicolaySerialSFM3x00
             _rxParser = new RxParser(OnParsed);
         }
 
-        public void Connect()
+        public Task Connect()
         {
+            if (_startTcs != null)
+            {
+                throw new InvalidOperationException("Device is already connected");
+            }
+            _startTcs = new TaskCompletionSource<bool>();
+            
             _serialPort = new SerialPort(_portName, 115200, Parity.None, 8, StopBits.One);
-            _serialPort.DataReceived += OnRx;
             _serialPort.Open();
+            
+            // Create long-running background task to read data
+            Task.Factory.StartNew(ReadTask, TaskCreationOptions.LongRunning);
+            return _startTcs.Task;
         }
-
+        
         /// <summary>
         /// Run the device's test (0x05) command and check the response
         /// </summary>
-        public void Check()
+        public Task<bool> Check()
         {
+            _checkTcs = new TaskCompletionSource<bool>();
             SendCommand(0x05, Array.Empty<byte>());
+            return _checkTcs.Task;
         }
 
         public async Task<int> GetValue()
@@ -44,15 +62,10 @@ namespace NicolaySerialSFM3x00
             return await _measurementTcs.Task;
         }
 
-        private void OnRx(object sender, SerialDataReceivedEventArgs e)
-        {
-            int bytesToRead = _serialPort.BytesToRead;
-            _serialPort.Read(_rxBuffer, 0, bytesToRead);
-            _rxParser.AddBytes(_rxBuffer[..bytesToRead]);
-        }
-
         private void OnParsed(byte[] message)
         {
+            if (message[0] != _address) return; // Not for us
+            
             if (message.Length < 4) return;
             // Console.WriteLine("Received Data: " + BitConverter.ToString(message));
             
@@ -64,8 +77,20 @@ namespace NicolaySerialSFM3x00
                 return;
             }
             
+            if (message[1] == 0x05)
+            {
+                if (message.Length != 6) 
+                {
+                    _checkTcs?.SetResult(false);
+                }
+                else
+                {
+                    _checkTcs?.SetResult(message[2] == 0x02 && message[3] == 0x55 && message[4] == 0xAA && message[5] == 0x7D);
+                }
+                return;
+            }
+            
         }
-        
         
         private void SendCommand(byte command, byte[] data)
         {
@@ -84,8 +109,43 @@ namespace NicolaySerialSFM3x00
 
             _serialPort.Write(packet, 0, length);
         }
+        
+        private Task StopReading()
+        {
+            if (_stopReadTcs == null) return Task.CompletedTask;
+            _cts.Cancel();
+            return _stopReadTcs.Task;
+        }
 
-        private byte Crc8(ReadOnlySpan<byte> data)
+        private async void ReadTask()
+        {
+            _stopReadTcs = new TaskCompletionSource<bool>();
+            _startTcs?.TrySetResult(true);
+            
+            try
+            {
+                var buffer = new byte[1024];
+                while (!_cts.Token.IsCancellationRequested)
+                {
+                    var bytesRead = await _serialPort.BaseStream.ReadAsync(buffer, _cts.Token);
+                    if (bytesRead > 0)
+                    {
+                        _rxParser.AddBytes(buffer.AsSpan(0, bytesRead));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw; // TODO handle exception
+            }
+            finally
+            {
+                _stopReadTcs.SetResult(true);
+                _stopReadTcs = null;
+            }
+        }
+        
+        private static byte Crc8(ReadOnlySpan<byte> data)
         {
             byte crc = 0x00;
             foreach (var b in data)
@@ -102,10 +162,20 @@ namespace NicolaySerialSFM3x00
             return crc;
         }
 
-        public void Dispose()
+
+        public async void Dispose()
         {
-            _serialPort?.DataReceived -= OnRx;
+            await StopReading();
+            _serialPort?.Close();
             _serialPort?.Dispose();
         }
+        
+        public async ValueTask DisposeAsync()
+        {
+            await StopReading();
+            _serialPort?.Close();
+            _serialPort?.Dispose();
+        }
+        
     }
 }
